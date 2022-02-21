d8b2a7cc0c788620a12c2305d0998b623581f3c6
hjarme <100164283+hjarme@users.noreply.github.com>
2022-02-22 04:28:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+        </head>
+        <body>
+        <h1>Welcome to My Website! I am Hershey L. Jarme!<h1>
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="/LT2/index.html">LEARNING TASK 2
