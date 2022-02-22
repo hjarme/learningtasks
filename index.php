db3ac2574bbff7b61cc8920ae8d6008e3e247f18
hjarme <100164283+hjarme@users.noreply.github.com>
2022-02-22 22:01:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <title>My Home Page</title>
         </head>
         <body>
-        <h1>Welcome to My Website! I am Hershey L. Jarme!<h1>
+        <h1>Welcome to My Website! I am Hershey L. Jarme :) <h1>
         <ul>
             <li><a href="index.php">HOME</a></li>
             <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
