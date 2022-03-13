8a4d5a3a8ddaa9edea6c0e31831c9b27f86294e0
hjarme <100164283+hjarme@users.noreply.github.com>
2022-03-14 02:09:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
-      href="/LT3/index.html</a></li>
-       href="/stylesheet.css</a><li>
-</html>
+<head>
+<link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
 
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
