2e4868133f52a664c20acd980a75804162444edc
hjarme <100164283+hjarme@users.noreply.github.com>
2022-03-14 01:40:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <html>
-    <li><a href="/LT3/index.html<li>
+     <head>
+         <title> My Top 10 Favorite Marvel Movies </title>
+     </head>
+    <body>
+        <h1>My Top 10 Favorite Marvel Movies </h1>
+        <ul><a href="index.php"HOME</href>
+            <li><a href="/LT3/index.html</a></li>
+        </ul>
+                </body>
     
 </html>
