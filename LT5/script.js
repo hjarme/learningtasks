91988c900c7ea3efc322712eda66efe4d32588c6
hjarme <100164283+hjarme@users.noreply.github.com>
2022-05-25 00:20:35 +0800

    Add files via upload

---
+++ LT5/script.js
@@ -0,0 +1,64 @@
+ function talkid(obj) {
+     if (obj.id === 'block-1') {
+         const myElement = document.getElementById('spongebobtalk');
+             myElement.style.display = "block";
+     } else if (obj.id === 'block-2'){
+         const myElement = document.getElementById('patricktalk');
+            myElement.style.display = "block";       
+    } else if (obj.id === 'block-3'){
+         const myElement = document.getElementById('sandytalk');
+            myElement.style.display = "block";       
+      } else if (obj.id === 'block-4'){
+         const myElement = document.getElementById('planktontalk');
+            myElement.style.display = "block";       
+      } else if (obj.id === 'block-5'){
+         const myElement = document.getElementById('mrkrabstalk');
+            myElement.style.display = "block";       
+      } else if (obj.id === 'block-6'){
+         const myElement = document.getElementById('pearlkrabstalk');
+            myElement.style.display = "block";       
+      }  else if (obj.id === 'block-7'){
+         const myElement = document.getElementById('kingnetptunetalk');
+            myElement.style.display = "block";       
+      } else if (obj.id === 'block-8'){
+         const myElement = document.getElementById('squidwardtalk');
+            myElement.style.display = "block";       
+      } else if (obj.id === 'block-9'){
+         const myElement = document.getElementById('garytalk');
+            myElement.style.display = "block";       
+      }
+    } 
+ 
+
+function hideid(obj) {
+    if (obj.id === 'block-1') {
+        const myElement = document.getElementById('spongebobtalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-2') {
+        const myElement = document.getElementById('patricktalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-3') {
+        const myElement = document.getElementById('sandytalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-4') {
+        const myElement = document.getElementById('planktontalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-5') {
+        const myElement = document.getElementById('mrkrabstalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-6') {
+        const myElement = document.getElementById('pearlkrabstalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-7') {
+        const myElement = document.getElementById('kingnetptunetalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-8') {
+        const myElement = document.getElementById('squidwardtalk');
+            myElement.style.display = "none";
+    } else if (obj.id === 'block-9') {
+        const myElement = document.getElementById('garytalk');
+            myElement.style.display = "none";
+    }
+}
+
+
